37f3841dda9a71923b237bb5e121300bec871422
Jason Hoang <jasonhoang6201@gmail.com>
2025-11-19 18:20:53 +0700

    feat: test env config

--- src/main/java/com/example/hello_spring_boot/config/ApplicationInitConfig.java
+++ src/main/java/com/example/hello_spring_boot/config/ApplicationInitConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +23,7 @@ public class ApplicationInitConfig {
     PasswordEncoder passwordEncoder;
 
     @Bean
+    @ConditionalOnProperty(prefix = "spring", value = "dev", matchIfMissing = false)
     ApplicationRunner applicationRunner(UserRepository userRepository) {
         return args -> {
             if (userRepository.existsByUsername("admin")) return;
